db71bc8f6168bc3bd61adf466885447afa9fae03
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-22 16:12:52 +0530

    submitting code-Thu Jun 22 2023 16:12:51 GMT+0530 (India Standard Time)

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,24 +4,25 @@ const Modal = (props) => {
   return (
     <>
       <button
-        className="model-overlay"
         onClick={() => {
           props.callback();
         }}
       >
         Show Modal
       </button>
-      {/* <div style={{ display: props.show ? "block" : "none" }}>
-        <button
-          className="model-close"
-          onClick={() => {
-            props.callback();
-          }}
-        >
-          Close
-        </button>
-        <Para />
-      </div> */}
+      { props.show &&
+        <div className="model-overlay">
+          <button
+            className="model-close"
+            onClick={() => {
+              props.callback();
+            }}
+          >
+            Close
+          </button>
+          <Para />
+        </div>
+      }
     </>
   );
 };
